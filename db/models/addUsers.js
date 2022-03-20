ad2bc52f70beb1d266daa91cfdf18856111bca28
Nathan Gaffley <nathangaffley@Nathans-MacBook-Pro.local>
2022-03-20 12:20:57 +0200

    created DB user add script

---
+++ db/models/addUsers.js
@@ -0,0 +1,31 @@
+const client = require('../dbConnection');
+const { faker } = require('@faker-js/faker');
+
+
+async function init() {
+    try {
+        await client.connect();
+        console.log('Connected successfully to server');
+    
+        const database = await client.db('client').collection('users');
+        const result = await database.insertOne({
+            _id: 10,
+            txtFirstName: faker.name.firstName(),
+            txtLastName: faker.name.lastName(),
+            txtUserName: faker.name.firstName() + faker.name.lastName(),
+            txtEmail: faker.internet.email(),
+            txtCellPhone: faker.phone.phoneNumber(),
+            txtPassword: faker.internet.password(),
+        });
+    
+        console.log(`Inserted ${result} record`);
+    
+    } catch (e) {
+        console.error(`Unable to get database collection: ${e}`);
+    } finally {
+        console.log('Mongo Connection is closing ~~~~');
+        await client.close();
+    }
+}
+
+init();
